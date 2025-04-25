486c934f6c77706a88330603f309a41ad99ce860
giabao1340 <luonggiabao060904@gmail.com>
2025-04-26 00:02:09 +0700

    create popular item

---
+++ lib/popular_item.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class PopularItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String location;
+  final double rating;
+  final int reviews;
+
+  const PopularItem({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.location,
+    required this.rating,
+    required this.reviews,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10),
+      width: 150,
+      height: 230,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 150,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5),
+                Text(location),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.amber, size: 16),
+                    SizedBox(width: 5),
+                    Text('$rating ($reviews reviews)'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
